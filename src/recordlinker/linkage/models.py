6cc3772fb20b0cfcc9a8cb55a39ff03edafd2a5a
cbrinson-rise8 <127439654+cbrinson-rise8@users.noreply.github.com>
2024-09-20 16:33:01 -0400

    Feat/algorithm configuration schema (#31)
    ## Description
    In models.py add new classes for storing data related to the available
    algorithms to run.
    
    ```mermaid
    erDiagram
        Algorithm {
            int id
            bool is_default "a check should be added to guarentee that only 1 row in the table is marked as the default"
            string label "should be unique"
            string description
        }
    
        AlgorithmPass {
            int id
            int algorithm_id
            int[] blockingkeys "a list of values from the BlockingKey table"
            string[] evaluators "a list of matching functions and values to use"
            string rule "the evaluation rule function"
            float cluster_ratio
            json kwargs "extra parameters to pass to the evalator functions"
        }
    
        Algorithm ||--o{ AlgorithmPass: "has"
    ```
    
    ## Related Issues
    closes #13
    
    ## Additional Notes
    [Add any additional context or notes that reviewers should know about.]
    
    ## Checklist
    Please review and complete the following checklist before submitting
    your pull request:
    
    - [x] I have ensured that the pull request is of a manageable size,
    allowing it to be reviewed within a single session.
    - [x] I have reviewed my changes to ensure they are clear, concise, and
    well-documented.
    - [x] I have updated the documentation, if applicable.
    - [x] I have added or updated test cases to cover my changes, if
    applicable.
    - [x] I have minimized the number of reviewers to include only those
    essential for the review.
    - [x] I have notified teammates in the review thread to build awareness.
    
    ## Checklist for Reviewers
    Please review and complete the following checklist during the review
    process:
    
    - [ ] The code follows best practices and conventions.
    - [ ] The changes implement the desired functionality or fix the
    reported issue.
    - [ ] The tests cover the new changes and pass successfully.
    - [ ] Any potential edge cases or error scenarios have been considered.

--- src/recordlinker/linkage/models.py
+++ src/recordlinker/linkage/models.py
@@ -1,21 +1,21 @@
 import uuid
 
+from sqlalchemy import event
 from sqlalchemy import ForeignKey
 from sqlalchemy import JSON
 from sqlalchemy import orm
 from sqlalchemy import String
+from sqlalchemy import Text
 
 
 class Base(orm.DeclarativeBase):
     pass
-
 
 class Person(Base):
     __tablename__ = "mpi_person"
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
     internal_id: orm.Mapped[uuid.UUID] = orm.mapped_column(default=uuid.uuid4)
-
 
 class ExternalPerson(Base):
     __tablename__ = "mpi_external_person"
@@ -25,14 +25,12 @@ class ExternalPerson(Base):
     external_id: orm.Mapped[str] = orm.mapped_column(String(255))
     source: orm.Mapped[str] = orm.mapped_column(String(255))
 
-
 class Patient(Base):
     __tablename__ = "mpi_patient"
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
     person_id: orm.Mapped[int] = orm.mapped_column(ForeignKey("mpi_person.id"))
     data: orm.Mapped[dict] = orm.mapped_column(JSON)
-
 
 class BlockingKey(Base):
     __tablename__ = "mpi_blocking_key"
@@ -47,3 +45,49 @@ class BlockingValue(Base):
     patient_id: orm.Mapped[int] = orm.mapped_column(ForeignKey("mpi_patient.id"))
     blockingkey_id: orm.Mapped[int] = orm.mapped_column(ForeignKey("mpi_blocking_key.id"))
     value: orm.Mapped[str] = orm.mapped_column(String(50), index=True)
+
+class Algorithm(Base):
+    __tablename__ = "algorithm"
+
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    is_default: orm.Mapped[bool] = orm.mapped_column(default=False, index=True)
+    label: orm.Mapped[str] = orm.mapped_column(String(255), unique=True)
+    description: orm.Mapped[str] = orm.mapped_column(Text())
+
+def check_only_one_default(mapping, connection, target):
+    """
+    Check if there is already a default algorithm before inserting or updating.
+    If another default algorithm exists, an exception is raised to prevent the operation.
+
+    Parameters:
+    connection: The database connection being used for the operation.
+    target: The instance of the Algorithm class being inserted or updated.
+    
+    Raises:
+    ValueError: If another algorithm is already marked as default.
+    """
+
+    session = orm.Session.object_session(target)
+  
+    if target.is_default:
+        # ruff linting rule E712 ignored on this line. 
+        # ruff wants to enforce using the 'is' operator over '=='. 
+        # However since we only want to compare the truth value of the SQL query result we need to use '=='.
+        existing = session.query(Algorithm).filter(Algorithm.is_default == True).first()    # noqa: E712
+      
+        if existing and existing.id != target.id:
+            raise ValueError("There can only be one default algorithm")
+
+event.listen(Algorithm, 'before_insert', check_only_one_default)
+event.listen(Algorithm, 'before_update', check_only_one_default)
+
+class AlgorithmPass(Base):
+    __tablename__ = "algorithm_pass"
+
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    algorithm_id: orm.Mapped[int] = orm.mapped_column(ForeignKey("algorithm.id"))
+    blocking_keys: orm.Mapped[list[int]] = orm.mapped_column(JSON)
+    evaluators: orm.Mapped[list[str]] = orm.mapped_column(JSON)     
+    rule: orm.Mapped[str] = orm.mapped_column(String(255))
+    cluster_ratio: orm.Mapped[float]
+    kwargs: orm.Mapped[dict] = orm.mapped_column(JSON)
