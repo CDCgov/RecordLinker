31e480a4b889b9531fba4751003eb55f70a8caaa
Eric Buckley <eric.buckley@gmail.com>
2024-09-23 20:32:05 -0700

    moving binding functions to top level utils package

--- src/recordlinker/utils.py
+++ src/recordlinker/utils.py
@@ -137,3 +137,45 @@ def _clean_up(dal: DataAccessLayer | None = None) -> None:
         pg_connection.execute(text("""DROP TABLE IF EXISTS public.pyway CASCADE;"""))
         pg_connection.commit()
         pg_connection.close()
+
+
+def bind_functions(data: dict) -> dict:
+    """
+    Binds the functions in the data to the functions in the module.
+    """
+    def _eval_non_list(data):
+        if isinstance(data, dict):
+            return bind_functions(data)
+        elif isinstance(data, str) and data.startswith("func:"):
+            return str_to_callable(data)
+        return data
+
+    bound = copy.copy(data)
+    for key, value in bound.items():
+        if isinstance(value, list):
+            bound[key] = [_eval_non_list(item) for item in value]
+        else:
+            bound[key] = _eval_non_list(value)
+    return bound
+
+
+def str_to_callable(val: str) -> typing.Callable:
+    """
+    Converts a string representation of a function to the function itself.
+    """
+    # Remove the "func:" prefix
+    if val.startswith("func:"):
+        val = val[5:]
+    # Split the string into module path and function name
+    module_path, func_name = val.rsplit(".", 1)
+    # Import the module
+    module = importlib.import_module(module_path)
+    # Get the function from the module
+    return getattr(module, func_name)
+
+
+def func_to_str(func: typing.Callable) -> str:
+    """
+    Converts a function to a string representation of the function.
+    """
+    return f"func:{func.__module__}.{func.__name__}"
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -149,3 +149,64 @@ def test_run_migrations_migration_failure(patched_run_pyway):
     with pytest.raises(Exception):
         run_migrations()
     patched_run_pyway.assert_has_calls([mock.call("validate"), mock.call("migrate")])
+
+
+def test_bind_functions():
+    funcs = {
+        "first_name": "func:recordlinker.linkage.matchers.feature_match_fuzzy_string",
+        "last_name": "func:recordlinker.linkage.matchers.feature_match_exact",
+    }
+    assert utils.bind_functions(funcs) == {
+        "first_name": matchers.feature_match_fuzzy_string,
+        "last_name": matchers.feature_match_exact,
+    }
+
+    funcs = {
+        "blocks": [
+            {"value": "birthdate"},
+            {"value": "func:recordlinker.linkage.matchers.feature_match_exact"},
+        ]
+    }
+    assert utils.bind_functions(funcs) == {
+        "blocks": [
+            {"value": "birthdate"},
+            {"value": matchers.feature_match_exact},
+        ]
+    }
+
+    funcs = {
+        "nested": {
+            "first_name": "func:recordlinker.linkage.matchers.feature_match_fuzzy_string",
+            "last_name": "func:recordlinker.linkage.matchers.feature_match_exact",
+        }
+    }
+    assert utils.bind_functions(funcs) == {
+        "nested": {
+            "first_name": matchers.feature_match_fuzzy_string,
+            "last_name": matchers.feature_match_exact,
+        }
+    }
+
+
+def test_str_to_callable():
+    val = "func:recordlinker.linkage.matchers.feature_match_exact"
+    assert utils.str_to_callable(val) == matchers.feature_match_exact
+    val = "recordlinker.linkage.matchers.feature_match_exact"
+    assert utils.str_to_callable(val) == matchers.feature_match_exact
+    val = "recordlinker.unknown_module.unknown_function"
+    with pytest.raises(ImportError):
+        utils.str_to_callable(val)
+    val = "recordlinker.linkage.matchers.unknown_function"
+    with pytest.raises(AttributeError):
+        utils.str_to_callable(val)
+
+
+def test_func_to_str():
+    assert (
+        utils.func_to_str(matchers.feature_match_exact)
+        == "func:recordlinker.linkage.matchers.feature_match_exact"
+    )
+    assert (
+        utils.func_to_str(matchers.feature_match_fuzzy_string)
+        == "func:recordlinker.linkage.matchers.feature_match_fuzzy_string"
+    )
