3dc7037e612d7053fb71d3b9aab4cf5c82463463
Eric Buckley <eric.buckley@gmail.com>
2024-09-25 11:44:58 -0700

    fixing formatting

--- src/recordlinker/config.py
+++ src/recordlinker/config.py
@@ -6,13 +6,17 @@ import pydantic_settings
 
 class Settings(pydantic_settings.BaseSettings):
     model_config = pydantic_settings.SettingsConfigDict(
-        env_file='.env', env_file_encoding='utf-8',
+        env_file=".env",
+        env_file_encoding="utf-8",
     )
 
     db_uri: str = pydantic.Field(description="The URI for the MPI database")
-    # FIXME: A separate URI for testing is temporary, this is necessary right now because
-    # the old schema only works with postgresql.  Once the old schema is removed we can consolidate
-    test_db_uri: str = pydantic.Field(description="The URI for the MPI database to run tests against")
+    # FIXME: A separate URI for testing is temporary, this is necessary right now
+    # because the old schema only works with postgresql.  Once the old schema is
+    # removed we can consolidate
+    test_db_uri: str = pydantic.Field(
+        description="The URI for the MPI database to run tests against"
+    )
     connection_pool_size: typing.Optional[int] = pydantic.Field(
         description="The number of MPI database connections in the connection pool",
         default=5,
@@ -24,4 +28,4 @@ class Settings(pydantic_settings.BaseSettings):
     )
 
 
-settings = Settings()
+settings = Settings()  # type: ignore
