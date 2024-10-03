905916c3b4c1d177c7255438d4e1b57a402c2d12
Eric Buckley <eric.buckley@gmail.com>
2024-10-02 20:44:21 -0700

    moving session management to recordlinker.database

--- src/recordlinker/models/base.py
+++ src/recordlinker/models/base.py
@@ -1,25 +1,9 @@
-from sqlalchemy import create_engine
 from sqlalchemy import orm
 from sqlalchemy import types as sqltypes
-
-from recordlinker.config import settings
 
 
 class Base(orm.DeclarativeBase):
     pass
-
-
-def get_session() -> orm.Session:
-    """
-    Creates a new session to the MPI database and returns it.
-    """
-    engine = create_engine(
-        url=settings.db_uri,
-        pool_size=settings.connection_pool_size,
-        max_overflow=settings.connection_pool_max_overflow,
-    )
-    Base.metadata.create_all(engine)
-    return orm.Session(engine)
 
 
 def get_bigint_pk():
