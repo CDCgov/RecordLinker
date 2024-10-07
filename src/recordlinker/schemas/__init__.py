09722a233aa08d32a8b399d878d0ab05be614797
Eric Buckley <eric.buckley@gmail.com>
2024-10-07 16:47:28 -0700

    adding schemas.algorithms

--- src/recordlinker/schemas/__init__.py
+++ src/recordlinker/schemas/__init__.py
@@ -1,7 +1,11 @@
+from .algorithm import Algorithm
+from .algorithm import AlgorithmPass
 from .pii import Feature
 from .pii import PIIRecord
 
 __all__ = [
+    "Algorithm",
+    "AlgorithmPass",
     "Feature",
     "PIIRecord",
 ]
