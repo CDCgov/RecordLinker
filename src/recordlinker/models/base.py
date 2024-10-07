119fc5dde8f731765758e54e5ab0db02ce192e94
Administrator <gitlab_admin_731e3f@example.com>
2024-10-07 15:15:09 -0600

    change base.py for test

--- src/recordlinker/models/base.py
+++ src/recordlinker/models/base.py
@@ -12,3 +12,4 @@ def get_bigint_pk():
     does not support it.  Thus for the SQLite dialect, we need to use INTEGER instead.
     """
     return sqltypes.BigInteger().with_variant(sqltypes.INTEGER, "sqlite")
+
