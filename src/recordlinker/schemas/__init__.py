8e5372d39be8f8179fe5f7ada0ec42a649ea460a
Eric Buckley <eric.buckley@gmail.com>
2024-10-07 12:17:50 -0700

    removing schema/algorithm reference for now

--- src/recordlinker/schemas/__init__.py
+++ src/recordlinker/schemas/__init__.py
@@ -1,11 +1,7 @@
-from .algorithm import Algorithm
-from .algorithm import AlgorithmPass
 from .pii import Feature
 from .pii import PIIRecord
 
 __all__ = [
-    "Algorithm",
-    "AlgorithmPass",
     "Feature",
     "PIIRecord",
 ]
