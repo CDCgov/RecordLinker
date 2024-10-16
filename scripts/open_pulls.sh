0a032a4de6828ca5614202ba9b4434656fdb3461
Eric Buckley <eric.buckley@gmail.com>
2024-10-15 17:45:57 -0700

    overriding github repo in CI

--- scripts/open_pulls.sh
+++ scripts/open_pulls.sh
@@ -14,9 +14,13 @@ if [ -z "$GITHUB_TOKEN" ]; then
     echo "Missing environment variable: GITHUB_TOKEN"
     exit 1
 fi
+
+if [ -z "$GITHUB_REPO" ]; then
+    REMOTE=$(git config --get remote.origin.url | sed -E 's#git@github.com:(.*)\.git#\1#')
+    GITHUB_REPO="$(echo $REMOTE | cut -d'/' -f1)/$(echo $REMOTE | cut -d'/' -f2)"
+fi
 # set the URL to the GitHub API
-REPO=$(git config --get remote.origin.url | sed -E 's#git@github.com:(.*)\.git#\1#')
-URL="https://api.github.com/repos/$(echo $REPO | cut -d'/' -f1)/$(echo $REPO | cut -d'/' -f2)"
+URL="https://api.github.com/repos/${GITHUB_REPO}"
 
 # create function to call GH API, the first argument is the endpoint
 gh_api() {
@@ -24,7 +28,7 @@ gh_api() {
 }
 
 # List all open non-draft pull requests, and reduce to key fields
-pr_list=$(gh_api pulls?state=open | jq -r 'map(select(.draft == false))' | jq '[.[] | {title, number, user: .user.login, html_url}]')
+pr_list=$(gh_api pulls?state=open | jq -r 'map(select(.draft == false))' | jq '[.[] | {title, number, user: .user.login, html_url, created_at}]')
 
 # For each PR, get the timestamp of the ready_for_review event using the timeline API
 # create an array of JSON objects
@@ -34,6 +38,10 @@ while IFS= read -r pr; do
   # get the timestamp of the ready_for_review event
   ready_for_review=$(gh_api issues/${number}/timeline | jq -r 'map(select(.event == "ready_for_review")) | .[0].created_at')
   # calculate the number of days since the PR was ready for review
+  # only calculate if ready_for_review does not equal "null"
+  if [ "$ready_for_review" == "null" ]; then
+      ready_for_review=$(echo $pr | jq -r '.created_at')
+  fi
   time_since=$((($(date +%s) - $(date -d "$ready_for_review" +%s)) / 86400))
   # add the time_since field to the JSON object
   pr=$(echo "$pr" | jq ". + {time_since: $time_since}")
