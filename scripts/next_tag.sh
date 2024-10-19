def79701cc2f8af064889686c97fe05c7ebb00f2
Eric Buckley <eric.buckley@gmail.com>
2024-10-18 21:13:12 -0700

    a few more changes

--- scripts/next_tag.sh
+++ scripts/next_tag.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#
+# Get the next feature version tag for the project.
+#
+# Usage: scripts/next_tag.sh
+#
+# The format of the tag is vYY.X.0-rc.Z, where:
+# - YY is the current two digit year
+# - X is the next feature version this year
+# - Z is the number of commits on main since the last tag
 
 
 # Get the current two digit year
@@ -7,10 +16,8 @@ year=$(date +"%y")
 latest_tag=$(git describe --tags --match "v*" --abbrev=0 $(git rev-list --tags --max-count=1) 2>/dev/null || echo "")
 # Count the number of commits on main since the last tag
 commits=$(git rev-list --count $latest_tag..HEAD --)
-# Get latest tag for the current year
+# Get latest tag for the current year, or default to v0.0.0
 latest_tag_for_year=$(git describe --tags --match "v${year}.*" --abbrev=0 $(git rev-list --tags --max-count=1) 2>/dev/null || echo "v0.0.0")
 # Get the next feature version
 next_feature_ver=$(($(echo $latest_tag_for_year | cut -d '.' -f 2) + 1))
-# Pad the next feature version
-next_feature_ver_padded=$(printf "%02d" "$next_feature_ver")
-echo "v${year}.${next_feature_ver_padded}.00-rc.${commits}"
+echo "v${year}.${next_feature_ver}.0-rc.${commits}"
