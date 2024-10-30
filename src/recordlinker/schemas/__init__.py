65f95630254d30090b91f55d1de6217db3650e55
Eric Buckley <eric.buckley@gmail.com>
2024-10-30 09:20:27 -0700

    link router (#99)
    ## Description
    Removing linking specific API endpoints out of main and into
    `recordlinker.routes.link_router`
    
    ## Additional Notes
    This is just a code reorg change, no net new functionality has been
    added.
    - Create a new router for the linking API endpoints
    - Create a new schema, `recordlinker.schemas.link` to store link
    request/response payload signatures
    - Remove `recordlinker.base_service` and define new names, versions,
    descriptions for the Record Linker app in `recordlinker.main`
    - renamed the FHIR api endpoint from `/link-record` to `/link/fhir`
    - Removed the sample FHIR bundles for the FHIR endpoint

--- src/recordlinker/schemas/__init__.py
+++ src/recordlinker/schemas/__init__.py
@@ -1,6 +1,10 @@
 from .algorithm import Algorithm
 from .algorithm import AlgorithmPass
 from .algorithm import AlgorithmSummary
+from .link import LinkFhirInput
+from .link import LinkFhirResponse
+from .link import LinkInput
+from .link import LinkResponse
 from .mpi import PatientPersonRef
 from .mpi import PersonRef
 from .pii import Feature
@@ -12,6 +16,10 @@ __all__ = [
     "AlgorithmSummary",
     "Feature",
     "PIIRecord",
+    "LinkInput",
+    "LinkResponse",
+    "LinkFhirInput",
+    "LinkFhirResponse",
     "PersonRef",
     "PatientPersonRef",
 ]
