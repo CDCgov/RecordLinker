7101038e533f967287bbb6f6c653ea7452210722
Eric Buckley <eric.buckley@gmail.com>
2024-11-01 12:45:15 -0700

    adding version number ot the docs

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -10,7 +10,9 @@ set -e
 cd "$(dirname "$0")/.."
 
 OUT=${1:-_site}
+VERSION=${VERSION:-$(python -c "from recordlinker._version import __version__; print(f'v{__version__}');")}
+SITE_NAME="RecordLinker Documentation (${VERSION})"
 
-mkdocs build --config-file docs/mkdocs.yml -d "../${OUT}"
+SITE_NAME=${SITE_NAME} mkdocs build --config-file docs/mkdocs.yml -d "../${OUT}"
 python -m recordlinker.utils.openapi_schema > ${OUT}/openapi.json
 npx  @redocly/cli build-docs -o "${OUT}/api-docs.html" "${OUT}/openapi.json"
