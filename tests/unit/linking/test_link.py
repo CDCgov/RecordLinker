e15af3ec9ee5c54b5212185a656755f3c5468b2b
Alex Hayward <44384574+alhayward@users.noreply.github.com>
2024-11-27 08:21:51 -0800

    style(matchers): rename matchers funcs for clarity (#137)
    ## Description
    * Rename feature matching and evaluation functions for clarity
    
        **Feature Matching**
        * `feature_match_any` -> `compare_match_any`
        * `feature_match_exact` -> `compare_match_all`
        * `feature_match_fuzzy_string` -> `compare_fuzzy_match`
    * `feature_match_log_odds_fuzzy_compare` ->
    `compare_probabilistic_fuzzy_match`
    
        **Evaluation**
        * `eval_perfect_match` -> `rule_match`
        * `eval_log_odds_cutoff` -> `rule_probabilistic_match`
    
    ## Related Issues
    closes #109 
    
    ---------
    
    Co-authored-by: cbrinson-rise8 <127439654+cbrinson-rise8@users.noreply.github.com>

--- tests/unit/linking/test_link.py
+++ tests/unit/linking/test_link.py
@@ -50,10 +50,10 @@ class TestCompare:
             algorithm_id=1,
             blocking_keys=[1],
             evaluators=[
-                {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.feature_match_exact"},
-                {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.feature_match_fuzzy_string"},
+                {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.compare_match_all"},
+                {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.compare_fuzzy_match"},
             ],
-            rule="func:recordlinker.linking.matchers.eval_perfect_match",
+            rule="func:recordlinker.linking.matchers.rule_match",
             kwargs={},
         )
 
@@ -89,10 +89,10 @@ class TestCompare:
             algorithm_id=1,
             blocking_keys=[1],
             evaluators=[
-                {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.feature_match_exact"},
-                {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.feature_match_exact"},
+                {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.compare_match_all"},
+                {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.compare_match_all"},
             ],
-            rule="func:recordlinker.linking.matchers.eval_perfect_match",
+            rule="func:recordlinker.linking.matchers.rule_match",
             kwargs={},
         )
 
