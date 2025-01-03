362f18fc46d15e8d4b38ad493a561b755ac4ce80
Eric Buckley <eric.buckley@gmail.com>
2025-01-03 10:21:10 -0800

    reorder

--- scripts/local_server.sh
+++ scripts/local_server.sh
@@ -16,4 +16,4 @@ PORT=${1:-8000}
 # Start the API server, reloading the server with the source code changes.
 # Also specify the app directory to run the server based off the source code
 # and not whats been installed into the virtual environment.
-uvicorn recordlinker.main:app --reload --app-dir src/ --reload-dir src/ --port ${PORT}
+uvicorn recordlinker.main:app --app-dir src/ --reload --reload-dir src/ --port ${PORT}
