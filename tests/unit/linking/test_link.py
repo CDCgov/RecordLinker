cae79766eb24e0aa53e3f1df6d6c7bb507a3be99
bamader <49412165+bamader@users.noreply.github.com>
2025-03-24 15:07:59 -0400

    Handle missing fields during evaluation (#259)
    ## Description
    This PR adds more robust handling of comparisons between records in
    which one or more fields are missing data. If only a small amount of
    data is missing (fewer log-odds-worth than a specified percentage of the
    total possible), then missing comparisons are awarded partial weight to
    allow for data recovery and quality matches. However, if too much data
    is missing, the candidacy is automatically rejected.
    
    ## Related Issues
    Closes #235 
    
    ## Additional Notes
    Just changed some variable names from the ticket for clarity

--- tests/unit/linking/test_link.py
+++ tests/unit/linking/test_link.py
@@ -45,19 +45,26 @@ class TestCompare:
             }
         )
 
+        evaluators = [
+            {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
+            {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
+        ]
+        log_odds = {"FIRST_NAME": 6.85, "LAST_NAME": 6.35}
+        eval_fields = [e["feature"] for e in evaluators]
+        max_points = sum([log_odds[e] for e in eval_fields])
+        max_allowed_missingness_proportion = 0.5
+        missing_field_points_proportion = 0.5
+
         algorithm_pass = models.AlgorithmPass(
             id=1,
             algorithm_id=1,
             blocking_keys=[1],
-            evaluators=[
-                {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
-                {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
-            ],
+            evaluators=evaluators,
             rule="func:recordlinker.linking.matchers.rule_probabilistic_match",
-            kwargs={"log_odds": {"FIRST_NAME": 6.85, "LAST_NAME": 6.35}, "true_match_threshold": 12},
+            kwargs={"log_odds": log_odds, "true_match_threshold": 12},
         )
 
-        assert link.compare(rec, pat, algorithm_pass) is True
+        assert link.compare(rec, pat, max_points, max_allowed_missingness_proportion, missing_field_points_proportion, algorithm_pass, log_odds) is True
 
     def test_compare_no_match(self):
         rec = schemas.PIIRecord(
@@ -84,19 +91,25 @@ class TestCompare:
                 ]
             }
         )
+        evaluators = [
+            {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
+            {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
+        ]
+        log_odds = {"FIRST_NAME": 6.85, "LAST_NAME": 6.35}
+        eval_fields = [e["feature"] for e in evaluators]
+        max_points = sum([log_odds[e] for e in eval_fields])
+        max_allowed_missingness_proportion = 0.5
+        missing_field_points_proportion = 0.5
         algorithm_pass = models.AlgorithmPass(
             id=1,
             algorithm_id=1,
             blocking_keys=[1],
-            evaluators=[
-                {"feature": "FIRST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
-                {"feature": "LAST_NAME", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
-            ],
+            evaluators=evaluators,
             rule="func:recordlinker.linking.matchers.rule_probabilistic_match",
-            kwargs={"log_odds": {"FIRST_NAME": 6.85, "LAST_NAME": 6.35}, "true_match_threshold": 12.95},
+            kwargs={"log_odds": log_odds, "true_match_threshold": 12.95},
         )
 
-        assert link.compare(rec, pat, algorithm_pass) is False
+        assert link.compare(rec, pat, max_points, max_allowed_missingness_proportion, missing_field_points_proportion, algorithm_pass, log_odds) is False
 
     def test_compare_identifier_match(self):
         rec = schemas.PIIRecord(
@@ -132,18 +145,25 @@ class TestCompare:
             }
         )
 
+        evaluators = [
+            {"feature": "IDENTIFIER", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"}
+        ]
+        log_odds = {"IDENTIFIER": 0.35}
+        eval_fields = [e["feature"] for e in evaluators]
+        max_points = sum([log_odds[e] for e in eval_fields])
+        max_allowed_missingness_proportion = 0.5
+        missing_field_points_proportion = 0.5
+
         algorithm_pass = models.AlgorithmPass(
             id=1,
             algorithm_id=1,
             blocking_keys=[1],
-            evaluators=[
-                {"feature": "IDENTIFIER", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
-            ],
+            evaluators=evaluators,
             rule="func:recordlinker.linking.matchers.rule_probabilistic_match",
-            kwargs={"log_odds": {"IDENTIFIER": 0.35}, "true_match_threshold": 0.3},
+            kwargs={"log_odds": log_odds, "true_match_threshold": 0.3},
         )
 
-        assert link.compare(rec, pat, algorithm_pass) is True
+        assert link.compare(rec, pat, max_points, max_allowed_missingness_proportion, missing_field_points_proportion, algorithm_pass, log_odds) is True
 
     def test_compare_identifier_with_suffix(self):
         rec = schemas.PIIRecord(
@@ -179,23 +199,30 @@ class TestCompare:
             }
         )
 
+        evaluators = [
+            {"feature": "IDENTIFIER", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"}
+        ]
+        log_odds = {"IDENTIFIER": 0.35}
+        eval_fields = [e["feature"] for e in evaluators]
+        max_points = sum([log_odds[e] for e in eval_fields])
+        max_allowed_missingness_proportion = 0.5
+        missing_field_points_proportion = 0.5
+
         algorithm_pass = models.AlgorithmPass(
             id=1,
             algorithm_id=1,
             blocking_keys=[1],
-            evaluators=[
-                {"feature": "IDENTIFIER:MR", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"},
-            ],
+            evaluators=evaluators,
             rule="func:recordlinker.linking.matchers.rule_probabilistic_match",
-            kwargs={"log_odds": {"IDENTIFIER": 0.35}, "true_match_threshold": 0.3},
+            kwargs={"log_odds": log_odds, "true_match_threshold": 0.3},
         )
 
         #should pass as MR is the same for both
-        assert link.compare(rec, pat, algorithm_pass) is True
+        assert link.compare(rec, pat, max_points, max_allowed_missingness_proportion, missing_field_points_proportion, algorithm_pass, log_odds) is True
 
         algorithm_pass.evaluators = [{"feature": "IDENTIFIER:SS", "func": "func:recordlinker.linking.matchers.compare_probabilistic_fuzzy_match"}]
         #should fail as SS is different for both
-        assert link.compare(rec, pat, algorithm_pass) is False
+        assert link.compare(rec, pat, max_points, max_allowed_missingness_proportion, missing_field_points_proportion, algorithm_pass, log_odds) is False
 
     def test_compare_invalid_feature(self):
         rec = schemas.PIIRecord(
@@ -235,7 +262,7 @@ class TestCompare:
         )
 
         with pytest.raises(ValueError):
-            link.compare(rec, pat, algorithm_pass)
+            link.compare(rec, pat, 0.0, 0.5, 0.5, algorithm_pass, {})
 
 
 class TestLinkRecordAgainstMpi:
@@ -328,6 +355,142 @@ class TestLinkRecordAgainstMpi:
         #  finds greatest strength match and correctly assigns to larger cluster
         assert matches == [False, True, False, True, False, False, True]
         assert sorted(list(mapped_patients.values())) == [1, 1, 1, 4]
+    
+    def test_match_with_missing_field(
+            self,
+            session,
+            default_algorithm,
+            patients: list[schemas.PIIRecord]
+        ):
+        # Make a deep copy of the first patient, then delete some info
+        patients = [patients[0]]
+        duplicate = copy.deepcopy(patients[0])
+        duplicate.external_id = str(uuid.uuid4())
+        duplicate.name[0].family = ""
+        duplicate.address[0].line[0] = ""
+        patients.append(duplicate)
+
+        # Test whether we can successfully make a match if info is missing
+        default_algorithm.passes[0].kwargs["true_match_threshold"] = 9.5
+        matches: list[bool] = []
+        mapped_patients: dict[str, int] = collections.defaultdict(int)
+        for data in patients[:2]:
+            (_, person, results, _) = link.link_record_against_mpi(data, session, default_algorithm)
+            matches.append(bool(person and results))
+            mapped_patients[person.reference_id] += 1
+
+        # First patient inserted into empty MPI, no match
+        # Second patient blocks in each pass, is missing a field, but is allowed to match
+        assert matches == [False, True]
+        assert sorted(list(mapped_patients.values())) == [2]
+
+    def test_reject_too_many_missing_field(
+            self,
+            session,
+            default_algorithm,
+            patients: list[schemas.PIIRecord]
+        ):
+        # Make a deep copy of the first patient, then delete some info
+        patients = [patients[0]]
+        duplicate = copy.deepcopy(patients[0])
+        duplicate.external_id = str(uuid.uuid4())
+        duplicate.name[0].given[0] = ""
+        duplicate.address[0].line[0] = ""
+        patients.append(duplicate)
+
+        # Test whether too many missing points causes failure
+        default_algorithm.max_missing_allowed_proportion = 0.3
+        matches: list[bool] = []
+        mapped_patients: dict[str, int] = collections.defaultdict(int)
+        for data in patients[:2]:
+            (_, person, results, _) = link.link_record_against_mpi(data, session, default_algorithm)
+            matches.append(bool(person and results))
+            mapped_patients[person.reference_id] += 1
+
+        # First patient inserted into empty MPI, no match
+        # Second patient blocks in each pass but missing too much data, fails
+        assert matches == [False, False]
+
+    def test_both_missingness_params_zero(
+            self,
+            session,
+            default_algorithm,
+            patients: list[schemas.PIIRecord]
+        ):
+        # Make a deep copy of the first patient, then delete some info so
+        # that both blocks contain missing fields
+        patients = [patients[0]]
+        duplicate = copy.deepcopy(patients[0])
+        duplicate.external_id = str(uuid.uuid4())
+        duplicate.name[0].given[0] = ""
+        duplicate.address[0].line[0] = ""
+        patients.append(duplicate)
+
+        # We'll test lower log-odds cutoffs and show that even if a record
+        # would regularly have the points to match, it's disqualified if it
+        # violates the user missingness constraint.
+        default_algorithm.max_missing_allowed_proportion = 0.0
+        default_algorithm.missing_field_points_proportion = 0.0
+        default_algorithm.passes[0].kwargs["true_match_threshold"] = 4.0
+        default_algorithm.passes[1].kwargs["true_match_threshold"] = 4.0
+        matches: list[bool] = []
+        mapped_patients: dict[str, int] = collections.defaultdict(int)
+        for data in patients[:2]:
+            (_, person, results, _) = link.link_record_against_mpi(data, session, default_algorithm)
+            matches.append(bool(person and results))
+            mapped_patients[person.reference_id] += 1
+
+        # First patient inserted into empty MPI, no match
+        # Second patient blocks in each pass, has score to match, but missingness
+        # disqualifies it 
+        assert matches == [False, False]
+
+    def test_missing_field_points_exceed_max_missingness_fraction(
+            self,
+            session,
+            default_algorithm,
+            patients: list[schemas.PIIRecord]
+        ):
+        """
+        Tests for the edge case where missing_field_points_proportion is large
+        but max_missing_allowed_proportion is small. This could easily result
+        from a situation where a user trains an algorithm that has a number of
+        low-log-odds-weight fields (e.g. lots of points 2 or fewer) that they
+        don't wish to ignore, but they still want their comparison to be between
+        mostly complete records, e.g. not allowing more than 10% of field points
+        to be missing. 
+        """
+        # Make a deep copy of the first patient, then delete some info
+        patients = [patients[0]]
+        duplicate = copy.deepcopy(patients[0])
+        duplicate.external_id = str(uuid.uuid4())
+        duplicate.name[0].given[0] = ""
+        duplicate.address[0].line[0] = ""
+        patients.append(duplicate)
+
+        # Create scenario described above: each pass will have 10 total points,
+        # the missing field will represent a small number of these points, but
+        # the total result should still be disqualified
+        log_odds = {
+            "FIRST_NAME": 2.5, "LAST_NAME": 7.5, "BIRTHDATE": 7.5, "ADDRESS": 2.5
+        }
+        default_algorithm.max_missing_allowed_proportion = 0.2
+        default_algorithm.missing_field_points_proportion = 0.7
+        default_algorithm.passes[0].kwargs["true_match_threshold"] = 8.5
+        default_algorithm.passes[0].kwargs["log_odds"] = log_odds
+        default_algorithm.passes[1].kwargs["true_match_threshold"] = 8.5
+        default_algorithm.passes[1].kwargs["log_odds"] = log_odds
+        matches: list[bool] = []
+        mapped_patients: dict[str, int] = collections.defaultdict(int)
+        for data in patients[:2]:
+            (_, person, results, _) = link.link_record_against_mpi(data, session, default_algorithm)
+            matches.append(bool(person and results))
+            mapped_patients[person.reference_id] += 1
+
+        # First patient inserted into empty MPI, no match
+        # Second patient blocks in each pass, only gets a tiny bump overall from
+        # low-value missing field, but fails user's overall completeness constraint
+        assert matches == [False, False]
 
     def test_default_possible_match(
             self,
