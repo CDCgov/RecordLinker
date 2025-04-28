3e14cd5116d591ab4bec16444b628dbdc1ba160b
m-goggins <marcelle.goggins@gmail.com>
2025-04-28 14:32:04 -0700

    add link/unlink endpoints

--- src/api/recordlinker/routes/demo_router.py
+++ src/api/recordlinker/routes/demo_router.py
@@ -11,6 +11,7 @@ import typing
 import fastapi
 
 from recordlinker import schemas
+from recordlinker import session_store
 from recordlinker.utils import path as utils
 
 router = fastapi.APIRouter()
@@ -73,3 +74,91 @@ def get_match_review_records(
     if match_review_record is None:
         raise fastapi.HTTPException(status_code=fastapi.status.HTTP_404_NOT_FOUND)
     return match_review_record
+
+
+@router.post(
+    "/record/{patient_reference_id}/link",
+    summary="Link demo records for match review",
+)
+def link_match(
+    patient_reference_id: int,
+    response: fastapi.Response,
+) -> schemas.demo.MatchReviewRecord:
+    """
+    Link demo records for match review.
+    """
+
+    match_review_record = next(
+        (d for d in data if d["incoming_record"]["patient_id"] == patient_reference_id), None
+    )
+    if match_review_record is None:
+        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_404_NOT_FOUND)
+
+    # Update the linked status
+    match_review_record["linked"] = True
+    # Update the incoming record with the person_id from the potential match
+    match_review_record["incoming_record"]["person_id"] = match_review_record["potential_match"][0][
+        "person_id"
+    ]
+
+    # Save session (modifies the real response)
+    session_store.save_session(
+        response,
+        key=f"linked_status_{patient_reference_id}",
+        data=match_review_record,
+    )
+    return match_review_record
+
+
+@router.post(
+    "/record/{patient_reference_id}/unlink",
+    summary="Unlink demo records for match review",
+)
+def unlink_match(
+    patient_reference_id: int,
+    response: fastapi.Response,
+) -> schemas.demo.MatchReviewRecord:
+    """
+    Unlink demo records for match review.
+    """
+
+    match_review_record = next(
+        (d for d in data if d["incoming_record"]["patient_id"] == patient_reference_id), None
+    )
+    if match_review_record is None:
+        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_404_NOT_FOUND)
+
+    # Update the linked status
+    match_review_record["linked"] = False
+    # TODO: Remove potential match from the match_review_record since they were deemed not a match
+
+    # Save session
+    session_store.save_session(
+        response,
+        key=f"linked_status_{patient_reference_id}",
+        data=match_review_record,
+    )
+    return match_review_record
+
+
+@router.get(
+    "/record/{patient_reference_id}/linked_status",
+    summary="Check linked status from session",
+)
+def get_linked_status(
+    patient_reference_id: int,
+    request: fastapi.Request,
+) -> schemas.demo.MatchReviewRecord:
+    """
+    Get linked status from session.
+    """
+    session_data = session_store.load_session(
+        request,
+        key=f"linked_status_{patient_reference_id}",
+    )
+    if session_data is None:
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_404_NOT_FOUND, detail="No session data found."
+        )
+
+    return session_data
