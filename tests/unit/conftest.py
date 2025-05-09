1d1482a9b5633f4efe08a1932d7c5adcfd942bfa
Eric Buckley <eric.buckley@gmail.com>
2025-05-09 10:38:33 -0700

    undoing bad merge comment

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -57,19 +57,19 @@ def client():
             yield c
 
 
-# @pytest.fixture(scope="session", autouse=True)
-# def clean_test_database():
-    # """Fixture to clean up the sqlite test database file after the test suite."""
-    # db_file = None
-    # with database.get_test_session() as session:
-        # # test if session is sqlite
-        # if session.get_bind().dialect.name == "sqlite":
-            # db_file = session.get_bind().url.database
-    # # Yield control to the test suite
-    # yield
-    # # Cleanup logic: remove the database file if it exists
-    # if db_file and os.path.exists(db_file):
-        # os.remove(db_file)
+@pytest.fixture(scope="session", autouse=True)
+def clean_test_database():
+    """Fixture to clean up the sqlite test database file after the test suite."""
+    db_file = None
+    with database.get_test_session() as session:
+        # test if session is sqlite
+        if session.get_bind().dialect.name == "sqlite":
+            db_file = session.get_bind().url.database
+    # Yield control to the test suite
+    yield
+    # Cleanup logic: remove the database file if it exists
+    if db_file and os.path.exists(db_file):
+        os.remove(db_file)
 
 
 @functools.lru_cache
