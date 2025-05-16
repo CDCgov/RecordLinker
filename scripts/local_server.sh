069608d90ff5cf9133c8e23311b9baf70344f62e
Eric Buckley <eric.buckley@gmail.com>
2025-05-16 08:28:07 -0700

    making default dev server port 8080 just like docker

--- scripts/local_server.sh
+++ scripts/local_server.sh
@@ -5,15 +5,15 @@
 # Usage: scripts/local_server.sh <port>
 #
 # <port> is the port on which to run the API server. If not specified, the server
-# will run on port 8000.
+# will run on port 8080.
 #
 set -e
 
 cd "$(dirname "$0")/.."
 
-PORT=${1:-8000}
+PORT=${1:-8080}
 
 # Start the API server, reloading the server with the source code changes.
 # Also specify the app directory to run the server based off the source code
 # and not whats been installed into the virtual environment.
-uvicorn recordlinker.main:app --app-dir src/ --reload --reload-dir src/ --port ${PORT}
+uvicorn recordlinker.main:app --app-dir src/ --reload --reload-dir src/ --port "${PORT}"
