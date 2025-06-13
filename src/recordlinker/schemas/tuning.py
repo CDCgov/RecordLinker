4c340c9e0518aba1442976a0aaa3a51f387112dd
Eric Buckley <eric.buckley@gmail.com>
2025-06-13 10:52:08 -0700

    adding tuning pydantic schemas

---
+++ src/recordlinker/schemas/tuning.py
@@ -0,0 +1,64 @@
+"""
+recordlinker.schemas.tuning
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module contains the schema definitions for the tuning jobs.
+"""
+
+import datetime
+import typing
+import uuid
+
+import pydantic
+from typing_extensions import Annotated
+
+from recordlinker import models
+from recordlinker.utils.datetime import now_utc
+
+from .algorithm import LogOdd
+
+
+class TuningParams(pydantic.BaseModel):
+    true_match_pairs: Annotated[int, pydantic.Field(gt=0)] = pydantic.Field(
+        description="The number of true match pairs to use for training."
+    )
+    non_match_pairs: Annotated[int, pydantic.Field(gt=0)] = pydantic.Field(
+        description="The number of non-match pairs to use for training."
+    )
+
+
+class TuningResults(pydantic.BaseModel):
+    dataset_size: Annotated[int, pydantic.Field(ge=0)] = pydantic.Field(
+        default=0, description="The number of records analyzed."
+    )
+    true_matches_found: Annotated[int, pydantic.Field(ge=0)] = pydantic.Field(
+        default=0, description="The number of true matches found."
+    )
+    non_matches_found: Annotated[int, pydantic.Field(ge=0)] = pydantic.Field(
+        default=0, description="The number of non-matches found."
+    )
+    log_odds: typing.Sequence[LogOdd] = []
+    details: str = pydantic.Field(
+        default="", description="Additional information about the tuning job."
+    )
+
+
+class TuningJob(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(from_attributes=True)
+
+    id: uuid.UUID = pydantic.Field(default_factory=uuid.uuid4)
+    status: models.TuningStatus = pydantic.Field(default=models.TuningStatus.PENDING)
+    params: TuningParams
+    results: typing.Optional[TuningResults] = None
+    started_at: datetime.datetime = pydantic.Field(default_factory=now_utc)
+    finished_at: typing.Optional[datetime.datetime] = None
+
+    @pydantic.computed_field  # type: ignore[misc]
+    @property
+    def duration(self) -> typing.Optional[datetime.timedelta]:
+        """
+        Get the duration of the tuning job.
+        """
+        if self.finished_at is None:
+            return None
+        return self.finished_at - self.started_at
