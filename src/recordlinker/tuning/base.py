bfc4100b5a46e8b2b821ee01e96fbde3bb4eb228
Eric Buckley <eric.buckley@gmail.com>
2025-06-16 22:00:18 -0700

    updating stub tune function to be async

--- src/recordlinker/tuning/base.py
+++ src/recordlinker/tuning/base.py
@@ -4,20 +4,29 @@ recordlinker.tuning.base
 
 This module provides functions for running log-odds tuning calculations
 """
+
+import contextlib
+import logging
+import typing
 import uuid
 
 from recordlinker import models
-from recordlinker.database import SessionMaker
+from recordlinker.database import get_session
 from recordlinker.database import tuning_service
 
+LOGGER = logging.getLogger(__name__)
 
-def tune(job_id: uuid.UUID) -> None:
+
+async def tune(job_id: uuid.UUID, session_factory: typing.Optional[typing.Callable] = None) -> None:
     """
     Run log-odds tuning calculations
     """
-    session = SessionMaker()
-    job = tuning_service.get_job(session, job_id)
-    if job is None:
-        raise ValueError(f"Tuning job not found: {job_id}")
-    tuning_service.update_job(session, job, models.TuningStatus.RUNNING)
-    # TODO: run log-odds tuning
+    LOGGER.info("tuning job received", extra={"job_id": job_id})
+    session_factory = session_factory or contextlib.contextmanager(get_session)
+    with session_factory() as session:
+        job = tuning_service.get_job(session, job_id)
+        if job is None:
+            LOGGER.error("tuning job not found", extra={"job_id": job_id})
+            raise ValueError(f"Tuning job not found: {job_id}")
+        tuning_service.update_job(session, job, models.TuningStatus.RUNNING)
+        # TODO: run log-odds tuning
