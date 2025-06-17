30169c246d5eb2a667d4626f5c87fb7138bf5147
Eric Buckley <eric.buckley@gmail.com>
2025-06-16 21:40:54 -0700

    adding tests for TZDateTime

--- tests/unit/models/test_tuning.py
+++ tests/unit/models/test_tuning.py
@@ -19,3 +19,36 @@ class TestTuningJob:
             assert job.duration.total_seconds() == 300
             job.finished_at = datetime.datetime(2025, 1, 1, 0, 2, 0)
             assert job.duration.total_seconds() == 120
+
+
+class TestTZDateTime:
+    def test_naive_datetime_becomes_utc(self, session):
+        naive_dt = datetime.datetime(2024, 1, 1, 12, 0, 0)  # Naive
+        obj = models.TuningJob(status=models.TuningStatus.PENDING, params={}, started_at=naive_dt)
+        session.add(obj)
+        session.commit()
+
+        obj = session.query(models.TuningJob).first()
+        assert obj.started_at.tzinfo is not None
+        assert obj.started_at.tzinfo.utcoffset(obj.started_at) == datetime.timedelta(0)
+
+    def test_aware_datetime_preserved_as_utc(self, session):
+        aware_dt = datetime.datetime(2024, 1, 1, 12, 0, 0, tzinfo=datetime.timezone.utc)
+        obj = models.TuningJob(status=models.TuningStatus.PENDING, params={}, started_at=aware_dt)
+        session.add(obj)
+        session.commit()
+
+        obj = session.query(models.TuningJob).first()
+        assert obj.started_at == aware_dt
+        assert obj.started_at.tzinfo == datetime.timezone.utc
+
+    def test_non_utc_converted_to_utc(self, session):
+        est = datetime.timezone(datetime.timedelta(hours=-5))
+        dt_est = datetime.datetime(2024, 1, 1, 7, 0, 0, tzinfo=est)
+        obj = models.TuningJob(status=models.TuningStatus.PENDING, params={}, started_at=dt_est)
+        session.add(obj)
+        session.commit()
+
+        obj = session.query(models.TuningJob).first()
+        assert obj.started_at.hour == 12
+        assert obj.started_at.tzinfo == datetime.timezone.utc
