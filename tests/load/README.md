c20c4e782304fbbac5097d4db1e913c2dcaf870a
Eric Buckley <eric.buckley@gmail.com>
2025-06-17 08:55:20 -0700

    Add additional load test options (#436)
    ## Description
    Adding more options to run load tests with, including the ability to
    toggle between `/link` and `/match`.
    
    ## Additional Notes
    - adding missing load testing dependencies to pyproject.toml
    - adding additional arguments to `scripts/gen_seed_test_data.py` to
    allow for data scrambling at creation time
    - adding a load testing README.md
    
    ---------
    
    Co-authored-by: m-goggins <marcelle.goggins@gmail.com>

---
+++ tests/load/README.md
@@ -0,0 +1,79 @@
+# Record Linker – Locust Load Testing
+
+This project includes a [Locust-based](https://locust.io/) load test suite for testing
+performance of the Record Linker API.
+
+## Setup
+
+### 1. Set up development environment. 
+
+Go through the development setup instructions in the [README](../README.md), this will install the necessary dependencies and set up a Python virtual environment.
+```sh
+source scripts/bootstrap.sh
+```
+
+### 2. Generate `record-data` file with sample records to send to the Record Linker API.
+
+The load tests requires 1 data file to simulate the test.  This file, called `record-data`, will contain  sample records in it, 
+    that the testing framework will use to send requests to the Record Linker API.
+This is called the `record-data` file.  You can use the [generate test data script](../scripts/gen_seed_test_data.py) to generate a sample file.  See that script for more details on how to create a test data file.
+```sh
+python scripts/gen_seed_test_data.py --count 100 > record-data.json
+```
+
+### 3. [OPTIONAL] Generate the `seed-data` file, which can be used to seed the database with records before the test starts.
+
+This is optional, as it's fine to start the test from an empty database.  Again, you can use the same [generate test data script](../scripts/gen_seed_test_data.py) to generate a test seed data file.
+```sh
+python scripts/gen_seed_test_data.py --count 9999 > seed-data.json
+```
+
+### 4. [OPTIONAL] Scramble the `record-data` file.
+
+A scrambling script has been included to randomize the data in the `record-data` file. See
+[scramble data script](scripts/scramble_data.py) for more details.
+
+### 5. Run an instance of the Record Linker API.
+
+The locust test suite will need an instance of the Record Linker API running.  This could be a local instance, or possibly over the network.  For a simple test, you can use the [local server script](../scripts/local_server.sh). See the [README](../README.md) for more details on a local setup and how to run the local server.
+```sh
+./scripts/local_server.sh
+```
+
+## Running the Locust Test Suite
+
+To run the Locust test for 10 seconds with a load of 2 users and 100 records to link, use the following command:
+
+```bash
+locust -f locustfile.py \
+    --headless \
+    -u 2 -r 2 \
+    --run-time 10s \
+    --records-to-link 100 \
+    --record-data assets/test_data.json \
+    --link-probability 0.5 \
+    --linkage-endpoint link
+```
+
+### Custom Parameters
+
+| Argument             | Type     | Default                 | Description                                         |
+|----------------------|----------|-------------------------|-----------------------------------------------------|
+| `--records-to-link`  | `int`    | `0`                     | Number of records to link in the test run, 0 for infinite. |
+| `--link-probability` | `float`  | `0.5`                   | Probability that any given record will be linked.  |
+| `--seed-data`        | `str`    | ` `                     | Path to the seed data file.                        |
+| `--record-data`      | `str`    | `assets/test_data.json` | Path to the record data file.                      |
+| `--linkage-endpoint` | `choice` | `match`                 | Which linkage endpoint to test, `match` or `link`  |
+
+### UI mode
+
+You can run the tests, and control the parameters, in the browser by removing the `--headless` flag.
+Visit http://localhost:8089 to view the UI.
+
+```sh
+locust -f locustfile.py
+```
+
+### 📍 Notes
+
+See `locust --help` for information on more options.
--- tests/load/locustfile.py
+++ tests/load/locustfile.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import pathlib
@@ -22,74 +23,109 @@ def str_to_bool(value):
         raise ValueError(f"Invalid boolean value: {value}")
 
 
+def normalized_value(val):
+    """
+    Helper function to convert a string representation of a float to a normalized value in
+    locust CLI.
+    """
+    try:
+        val = float(val)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{val} is not a valid float")
+    if val <= 0.0 or val > 1.0:
+        raise argparse.ArgumentTypeError(f"{val} not in range (0.0, 1.0]")
+    return val
+
+
 @locust.events.init_command_line_parser.add_listener
 def _(parser):
     parser.add_argument(
-        "--records-to-link", type=int, default=100, help="Number of records to link in the test run"
+        "--records-to-link",
+        type=int,
+        default=0,
+        help="Number of records to link in the test run, infinite if 0",
+        required=True,
     )
     parser.add_argument(
         "--link-probability",
-        type=float,
+        type=normalized_value,
         default=0.5,
         help="Probability of linking a record",
+        required=True,
     )
-
     parser.add_argument(
-        "--seeded",
-        type=str_to_bool,
-        default=False,
-        help="Indicates if the database has been seeded with initial data",
+        "--seed-data",
+        type=str,
+        help="Path to the record data file to use when seeding the database",
+    )
+    parser.add_argument(
+        "--record-data",
+        type=str,
+        default=f"{pathlib.Path(__file__).resolve().parent}/assets/test_data.json",
+        help="Path to the record data file to use in the test run",
+        required=True,
+    )
+    parser.add_argument(
+        "--linkage-endpoint",
+        choices=["match", "link"],
+        default="match",
+        help="The linkage endpoint to use in the test run",
+        required=True,
     )
 
 
 class LoadTest(locust.HttpUser):
-    def on_start(self):
-        # Check if the database has been seeded before starting the test
-        seeded = self.environment.parsed_options.seeded
+    host = "http://localhost:8080/api"
+    auto_name_stripping = False
 
-        if not seeded:
-            locust_file = pathlib.Path(__file__).resolve()
-            project_root = locust_file.parents[2]
-            seed_data_path = os.path.join(project_root, "tests/load/assets/seed_data.json")
-            with open(seed_data_path, "rb") as input_file:
+    def on_start(self):
+        options = self.environment.parsed_options
+        if options.seed_data:
+            if os.path.exists(options.seed_data):
+                raise ValueError(f"Seed data file does not exist: {options.seed_data}")
+
+            chunk_size = 100
+            with open(options.seed_data, "r") as input_file:
                 clusters_iter = ijson.items(input_file, "clusters.item", use_float=True)
                 chunk = []
-                chunk_size = 100
                 for cluster in clusters_iter:
                     chunk.append(cluster)
                     if len(chunk) == chunk_size:
-                        seed_data = json.dumps({"clusters": chunk}, indent=2)
-                        self.client.post("/api/seed", seed_data)
+                        self.client.post("/seed", json.dumps({"clusters": chunk}))
                         chunk = []
 
                 if chunk:
-                    seed_data = json.dumps({"clusters": chunk}, indent=2)
-                    self.client.post("/api/seed", seed_data)
+                    self.client.post("/seed", json.dumps({"clusters": chunk}))
 
     @locust.task
     def link(self):
         """
         Loops through the original data, randomly selects records, and links them.
         """
-        records_to_link = self.environment.parsed_options.records_to_link
+        options = self.environment.parsed_options
+        # check if string is a valid path
+        if not os.path.exists(options.record_data):
+            raise ValueError(f"Record data file does not exist: {options.record_data}")
 
-        # Pick a random record from the seed data
-        locust_file = pathlib.Path(__file__).resolve()
-        project_root = locust_file.parents[2]
-        original_data_path = os.path.join(project_root, "tests/load/assets/test_data_large.json")
-
-        with open(original_data_path, "rb") as input_file:
-            clusters_iter = ijson.items(input_file, "clusters.item", use_float=True)
+        with open(options.record_data, "r") as input_file:
+            url = f"/{options.linkage_endpoint}"
             counter = 0
-            for cluster in clusters_iter:
-                # decide whether to link or not
-                if random.random() < self.environment.parsed_options.link_probability:
-                    data = {
-                        "record": cluster["records"][0],
-                        "external_person_id": cluster["external_person_id"],
-                    }
+            for cluster in ijson.items(input_file, "clusters.item", use_float=True):
+                data = {"external_person_id": cluster.get("external_person_id", None)}
+                for record in cluster["records"]:
+                    # decide whether to link or not
+                    if random.random() < options.link_probability:
+                        counter += 1
+                        data["record"] = record
+                        with self.client.post(url, json=data, catch_response=True) as resp:
+                            try:
+                                grade = resp.json().get("match_grade", "unknown")
+                                # Override the request name based on match_grade
+                                resp.request_meta["name"] = f"match_grade::{grade}"
+                                resp.success()
+                            except Exception as e:
+                                resp.request_meta["name"] = "match_grade::error"
+                                resp.failure(str(e))
 
-                    self.client.post("/api/link", json=data)
-
-                    if counter >= records_to_link:
-                        self.environment.runner.quit()
+                        if options.records_to_link and counter >= options.records_to_link:
+                            self.environment.runner.quit()
