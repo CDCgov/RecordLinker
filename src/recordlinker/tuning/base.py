62615037436907cb010aff84837316cf0d4ec9d5
Eric Buckley <eric.buckley@gmail.com>
2025-06-20 16:02:43 -0700

    remove imports for now

--- src/recordlinker/tuning/base.py
+++ src/recordlinker/tuning/base.py
@@ -12,10 +12,7 @@ import uuid
 from recordlinker import models
 from recordlinker import schemas
 from recordlinker.database import get_session_manager
-from recordlinker.database import mpi_service
 from recordlinker.database import tuning_service
-
-from . import prob_calc
 
 LOGGER = logging.getLogger(__name__)
 
@@ -38,10 +35,10 @@ async def tune(job_id: uuid.UUID, session_factory: typing.Optional[typing.Callab
 
             # # CALCULATE M-PROBABILITIES
             # true_pairs: typing.Sequence[typing.Tuple[dict, dict]] = (
-                # mpi_service.generate_true_match_tuning_samples(
-                    # session,
-                    # job.params.true_match_pairs_requested,
-                # )
+            # mpi_service.generate_true_match_tuning_samples(
+            # session,
+            # job.params.true_match_pairs_requested,
+            # )
             # )
             # results.true_match_pairs_used = len(true_pairs)
             # m_probs: dict[str, float] = prob_calc.calculate_class_probs(true_pairs)
@@ -49,11 +46,11 @@ async def tune(job_id: uuid.UUID, session_factory: typing.Optional[typing.Callab
 
             # # CALCULATE U-PROBABILITIES
             # non_pairs: typing.Sequence[typing.Tuple[dict, dict]] = (
-                # mpi_service.generate_non_match_tuning_samples(
-                    # session,
-                    # sample_size=job.params.non_match_sample_requested,
-                    # n_pairs=job.params.non_match_pairs_requested,
-                # )
+            # mpi_service.generate_non_match_tuning_samples(
+            # session,
+            # sample_size=job.params.non_match_sample_requested,
+            # n_pairs=job.params.non_match_pairs_requested,
+            # )
             # )
             # results.non_match_pairs_used = len(non_pairs)
             # # TODO: set non_match_sample_used
@@ -61,10 +58,10 @@ async def tune(job_id: uuid.UUID, session_factory: typing.Optional[typing.Callab
             # del non_pairs
 
             # log_odds: dict[str, float] = prob_calc.calculate_log_odds(
-                # m_probs=m_probs, u_probs=u_probs
+            # m_probs=m_probs, u_probs=u_probs
             # )
             # results.log_odds = [
-                # schemas.LogOdd(feature=schemas.Feature.parse(k), value=v) for k, v in log_odds.items()
+            # schemas.LogOdd(feature=schemas.Feature.parse(k), value=v) for k, v in log_odds.items()
             # ]
             # # TODO: calculate pass recommendations
             tuning_service.update_job(session, job, models.TuningStatus.COMPLETED, results)
