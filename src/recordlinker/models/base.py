43e9b83b428c9ca1f889145e2a9efdaf21badedf
Eric Buckley <eric.buckley@gmail.com>
2025-06-23 11:13:26 -0700

    Add additional tuning data access functions (#445)
    ## Description
    Adding two additional DAL functions to support initiating a tuning job;
    `get_active_jobs` and `fail_job`.
    
    ## Related Issues
    #427 
    
    ## Additional Notes
    Additionally, a TZDateTime SQL type is introduced to ensure that columns
    of this type return timezone-aware datetime values.

--- src/recordlinker/models/base.py
+++ src/recordlinker/models/base.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy import orm
 from sqlalchemy import types as sqltypes
 
@@ -5,13 +7,13 @@ from recordlinker.config import settings
 
 
 class PrefixerMeta(orm.DeclarativeMeta):
-
     def __init__(cls, name, bases, dict_):
         name = dict_.get("__tablename__")
         if name:
             cls.__tablename__ = f"{settings.db_table_prefix}{name}"
             dict_["__tablename__"] = f"{settings.db_table_prefix}{name}"
         return super().__init__(name, bases, dict_)
+
 
 Base = orm.declarative_base(metaclass=PrefixerMeta)
 
@@ -22,3 +24,29 @@ def get_bigint_pk():
     does not support it.  Thus for the SQLite dialect, we need to use INTEGER instead.
     """
     return sqltypes.BigInteger().with_variant(sqltypes.INTEGER, "sqlite")
+
+
+class TZDateTime(sqltypes.TypeDecorator):
+    """
+    Custom DateTime type that ensures timezone-awareness (defaults to UTC).
+    Works like sqlalchemy.DateTime(timezone=False), but always returns tz-aware values.
+    """
+
+    impl = sqltypes.DateTime
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        """
+        Convert aware datetime to naive before storing if timezone=False
+        """
+        if value is not None and value.tzinfo is not None:
+            return value.astimezone(datetime.timezone.utc).replace(tzinfo=None)
+        return value
+
+    def process_result_value(self, value, dialect):
+        """
+        Ensure tz-aware datetime (assuming stored in UTC)
+        """
+        if value is not None and value.tzinfo is None:
+            return value.replace(tzinfo=datetime.timezone.utc)
+        return value
--- src/recordlinker/models/tuning.py
+++ src/recordlinker/models/tuning.py
@@ -1,12 +1,14 @@
+import datetime
 import enum
 import uuid
 
 from sqlalchemy import orm
 from sqlalchemy import types as sqltypes
 
-from recordlinker.utils.datetime import now_utc
+from recordlinker.utils.datetime import now_utc_no_ms
 
 from .base import Base
+from .base import TZDateTime
 
 
 class TuningStatus(enum.Enum):
@@ -26,15 +28,23 @@ class TuningJob(Base):
         sqltypes.Enum(TuningStatus, name="status_enum", native_enum=False),
         nullable=False,
     )
-    params: orm.Mapped[dict] = orm.mapped_column(sqltypes.JSON, default=dict)
-    results: orm.Mapped[dict] = orm.mapped_column(sqltypes.JSON, default=dict)
+    params: orm.Mapped[dict] = orm.mapped_column(sqltypes.JSON, nullable=False)
+    results: orm.Mapped[dict] = orm.mapped_column(sqltypes.JSON, default=None, nullable=True)
     started_at = orm.mapped_column(
-        sqltypes.DateTime,
-        default=now_utc,
+        TZDateTime,
+        default=now_utc_no_ms(),
         nullable=False,
     )
     finished_at = orm.mapped_column(
-        sqltypes.DateTime,
+        TZDateTime,
         default=None,
         nullable=True,
     )
+
+    @property
+    def duration(self) -> datetime.timedelta:
+        """
+        Get the duration of the tuning job.
+        """
+        last_ts: datetime.datetime = self.finished_at or now_utc_no_ms()
+        return last_ts - self.started_at
