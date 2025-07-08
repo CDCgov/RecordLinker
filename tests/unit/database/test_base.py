1ec22397a69128b0704678b9952916ba7832f935
Eric Buckley <eric.buckley@gmail.com>
2025-07-08 16:31:57 -0700

    get_random_function test cases

--- tests/unit/database/test_base.py
+++ tests/unit/database/test_base.py
@@ -4,11 +4,17 @@ unit.database.test_base.py
 
 This module contains the unit tests for the recordlinker.database module.
 """
+
 import os
 import unittest.mock
 
+import pytest
+from sqlalchemy.engine.interfaces import Dialect
+from sqlalchemy.sql import func
+
 from recordlinker.config import settings
 from recordlinker.database import create_sessionmaker
+from recordlinker.database import get_random_function
 from recordlinker.database import tables
 
 
@@ -46,3 +52,29 @@ def test_create_sessionmaker():
         os.remove("test.db")
     except FileNotFoundError:
         pass
+
+
+class TestGetRandomFunction:
+    class FakeDialect(Dialect):
+        "Fake dialect class for testing."
+        def __init__(self, name):
+            self.name = name
+
+    @pytest.mark.parametrize(
+        "dialect_name,expected_func",
+        [
+            ("sqlite", func.random),
+            ("postgresql", func.random),
+            ("mysql", func.rand),
+            ("mssql", func.newid),
+        ],
+    )
+    def test_known_dialects(self, dialect_name, expected_func):
+        dialect = self.FakeDialect(dialect_name)
+        result = get_random_function(dialect)
+        assert str(result).startswith(str(expected_func())), f"Failed for dialect: {dialect_name}"
+
+    def test_unsupported_dialect(self):
+        dialect = self.FakeDialect("oracle")
+        with pytest.raises(NotImplementedError, match="Unsupported DB dialect: oracle"):
+            get_random_function(dialect)
