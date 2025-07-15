d423546412a359a60124d595c27bc69a344a9b50
bamader <49412165+bamader@users.noreply.github.com>
2025-07-15 16:50:53 -0400

    Add robustness error processing (#467)
    ## Description
    This PR adds error handling and logger processing for a variety of
    issues that could be present in the MPI that might stop tuning from
    working properly. DB cluster structured is validated, as are request
    params.
    
    ## Related Issues
    #455

--- src/recordlinker/tuning/base.py
+++ src/recordlinker/tuning/base.py
@@ -32,7 +32,38 @@ async def tune(job_id: uuid.UUID, session_factory: typing.Optional[typing.Callab
         if job is None:
             LOGGER.error("tuning job not found", extra={"job_id": job_id})
             raise ValueError(f"Tuning job not found: {job_id}")
+        
+        # Some early fail-fasts if the user hasn't requested enough data to
+        # make tuning results meaningful
+        if job.params.true_match_pairs_requested < 1000:
+            LOGGER.error("too few true match pairs requested", extra={
+                "job_id": job_id, "true_match_pairs_requested": job.params.true_match_pairs_requested
+            })
+            raise ValueError(f"Too few true match pairs requested for job {job_id}: minimum permitted 1000, {job.params.true_match_pairs_requested} requested")
+        if job.params.non_match_sample_requested < 10000:
+            LOGGER.error("too few non match samples requested", extra={
+                "job_id": job_id, "non_match_samples_requested": job.params.non_match_sample_requested
+            })
+            raise ValueError(f"Too few non match samples requested for job {job_id}: minimum permitted 10000, {job.params.non_match_sample_requested} requested")
+
+        if job.params.non_match_pairs_requested < 1000:
+            LOGGER.error("too few non match pairs requested", extra={
+                "job_id": job_id, "non_match_pairs_requested": job.params.non_match_pairs_requested
+            })
+            raise ValueError(f"Too few non match pairs requested for job {job_id}: minimum permitted 1000, {job.params.non_match_pairs_requested} requested")
+
         try:
+            # Pre-flight checks: DB must be non-empty, have more than one Person 
+            # cluster, and not have a single monolith Person in order to tune
+            db_empty: bool = mpi_service.check_mpi_non_empty(session)
+            if db_empty:
+                LOGGER.error("no patient data in MPI", extra={"job_id": job_id})
+                raise ValueError("MPI contains no patient data")
+            acceptable_structure, unique_person_ids = mpi_service.check_mpi_has_acceptable_cluster_structure(session)
+            if not acceptable_structure:
+                LOGGER.error("MPI Person cluster structure invalid for tuning", extra={"job_id": job_id, "num_person_clusters_in_MPI": unique_person_ids})
+                raise ValueError(f"MPI has person structure that does not support tuning: must have num_person_clusters greater than 1 and less than num_patients, have {unique_person_ids}")
+
             tuning_service.update_job(session, job, models.TuningStatus.RUNNING)
             results: schemas.TuningResults = schemas.TuningResults()
 
@@ -44,11 +75,28 @@ async def tune(job_id: uuid.UUID, session_factory: typing.Optional[typing.Callab
                     job.params.true_match_pairs_requested,
                 )
             )
+            if len(true_pairs) < 3000:
+                LOGGER.warning(
+                    "Fewer than recommended true-match pairs found in MPI, proceed with caution",
+                    extra={"job_id": job_id, "true_match_pairs_found": len(true_pairs)}
+                )
+            
             non_pairs, sample_used = mpi_service.generate_non_match_tuning_samples(
                 session,
                 sample_size=job.params.non_match_sample_requested,
                 n_pairs=job.params.non_match_pairs_requested,
             )
+            if sample_used < 50000:
+                LOGGER.warning(
+                    "Lower than recommended negative sample used, proceed with caution",
+                    extra={"job_id": job_id, "negative_sample_used": sample_used}
+                )
+            if len(non_pairs) < 3000:
+                LOGGER.warning(
+                    "Fewer than recommended non-match pairs found in MPI, proceed with caution",
+                    extra={"job_id": job_id, "non_match_pairs_found": len(non_pairs)}
+                )
+
             results.true_match_pairs_used = len(true_pairs)
             results.non_match_pairs_used = len(non_pairs)
             results.non_match_sample_used = sample_used
@@ -69,26 +117,32 @@ async def tune(job_id: uuid.UUID, session_factory: typing.Optional[typing.Callab
 
             # Step 4: Compute suggested RMS possible match window boundaries
             obj: models.Algorithm | None = default_algorithm(session)
-            assert obj is not None
-            algorithm: schemas.Algorithm = schemas.Algorithm.model_validate(obj)
-            sorted_scores: dict[str, typing.Tuple[list[float], list[float]]] = (
-                prob_calc.calculate_and_sort_tuning_scores(
-                    true_pairs, non_pairs, log_odds, algorithm
+            if obj is not None:
+                algorithm: schemas.Algorithm = schemas.Algorithm.model_validate(obj)
+                sorted_scores: dict[str, typing.Tuple[list[float], list[float]]] = (
+                    prob_calc.calculate_and_sort_tuning_scores(
+                        true_pairs, non_pairs, log_odds, algorithm
+                    )
                 )
-            )
-            rms_bounds: dict[str, typing.Tuple[float, float]] = (
-                prob_calc.estimate_rms_bounds(sorted_scores)
-            )
-            pass_recs: list[schemas.PassRecommendation] = []
-            for idx, algorithm_pass in enumerate(algorithm.passes):
-                pass_name: str = algorithm_pass.label or f"pass_{idx}"  # type: ignore
-                rec = schemas.PassRecommendation(
-                    algorithm_label=algorithm.label,
-                    pass_label=pass_name,
-                    recommended_match_window=rms_bounds[pass_name]
+                rms_bounds: dict[str, typing.Tuple[float, float]] = (
+                    prob_calc.estimate_rms_bounds(sorted_scores)
                 )
-                pass_recs.append(rec)
-            results.passes = pass_recs
+                pass_recs: list[schemas.PassRecommendation] = []
+                for idx, algorithm_pass in enumerate(algorithm.passes):
+                    pass_name: str = algorithm_pass.label or f"pass_{idx}"  # type: ignore
+                    rec = schemas.PassRecommendation(
+                        algorithm_label=algorithm.label,
+                        pass_label=pass_name,
+                        recommended_match_window=rms_bounds[pass_name]
+                    )
+                    pass_recs.append(rec)
+                results.passes = pass_recs
+            # Can only make recommendation if default algo exists, otherwise warn and skip
+            else:
+                LOGGER.warning(
+                    "No default algorithm stored on-file, omitting RMS bound estimation",
+                    extra={"job_id": job_id}
+                )
 
             tuning_service.update_job(session, job, models.TuningStatus.COMPLETED, results)
         except Exception as exc:
